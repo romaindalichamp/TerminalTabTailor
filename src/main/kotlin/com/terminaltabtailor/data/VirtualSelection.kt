3ca3345decfaace35d9c1f6cda691a52e0aa1c46
Romain Dalichamp <romain.dalichamp@gmail.com>
2024-03-13 21:19:27 -0400

    refactor: refactor settings using Kotlin UI DSL Version 2

---
+++ src/main/kotlin/com/terminaltabtailor/data/VirtualSelection.kt
@@ -0,0 +1,11 @@
+package com.terminaltabtailor.data
+
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.vfs.VirtualFile
+
+data class VirtualSelection (
+    var lastSelectedVirtualFile: VirtualFile? = null,
+    var lastSelectedVirtualFileParent: VirtualFile? = null,
+    var lastSelectedVirtualFileParentModule: Module? = null,
+    var lastSelectedVirtualFileParentModuleDirName: String? = null,
+)
--- src/main/kotlin/com/terminaltabtailor/util/VirtualFilesUtil.kt
+++ src/main/kotlin/com/terminaltabtailor/util/VirtualFilesUtil.kt
@@ -6,7 +6,7 @@ import com.intellij.openapi.module.ModuleUtilCore
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ModuleRootManager
 import com.intellij.openapi.vfs.VirtualFile
-import com.terminaltabtailor.dto.VirtualSelection
+import com.terminaltabtailor.data.VirtualSelection
 
 class VirtualFilesUtil {
     companion object {
