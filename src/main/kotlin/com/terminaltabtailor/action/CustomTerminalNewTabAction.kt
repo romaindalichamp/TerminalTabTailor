3142bbffd2936beeac13b4d42ab0e9e3f4365b71
Romain Dalichamp <romain.dalichamp@gmail.com>
2024-03-23 00:04:10 -0400

    clean: CustomTerminalNewTabAction

--- src/main/kotlin/com/terminaltabtailor/action/CustomTerminalNewTabAction.kt
+++ src/main/kotlin/com/terminaltabtailor/action/CustomTerminalNewTabAction.kt
@@ -1,37 +1,14 @@
 package com.terminaltabtailor.action
 
-import com.intellij.icons.AllIcons
-import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.PlatformDataKeys
+import com.intellij.openapi.project.DumbAwareAction
 import com.terminaltabtailor.manager.TerminalTabNamesManager
-import org.jetbrains.plugins.terminal.TerminalToolWindowManager
-import org.jetbrains.plugins.terminal.exp.TerminalPromotedDumbAwareAction
 import java.util.*
-import javax.swing.Icon
 
 open class CustomTerminalNewTabAction(
     val text: String = ResourceBundle.getBundle("TerminalTabTailorBundle")
         .getString("action.customTerminalNewTab.text"),
-    private val description: String = ResourceBundle.getBundle("TerminalTabTailorBundle")
-        .getString("action.customTerminalNewTab.description"),
-    private val icon: Icon = AllIcons.General.Add,
-) : TerminalPromotedDumbAwareAction() {
-
-    init {
-        templatePresentation.also {
-            it.text = text
-            it.setDescription(description)
-            it.setIconSupplier { icon }
-        }
-    }
-
-    override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
-
-    override fun update(e: AnActionEvent) {
-        val toolWindow = e.dataContext.getData(PlatformDataKeys.TOOL_WINDOW)
-        e.presentation.isEnabled = TerminalToolWindowManager.isTerminalToolWindow(toolWindow) && e.project != null
-    }
+) : DumbAwareAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
         TerminalTabNamesManager.openTabInTerminal(e)
